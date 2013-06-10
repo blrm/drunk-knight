95f26d920721a8ee70d82867a62b9f9887911f93
blair morrison <hashbang@betrayal.(none)>
2013-06-10 12:32:56 -0400

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to stupid-cannon
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/stupid_cannon/core_test.clj
@@ -0,0 +1,7 @@
+(ns stupid-cannon.core-test
+  (:use clojure.test
+        stupid-cannon.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
